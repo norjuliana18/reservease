dbfb990fc73490b8a9fced4bbb111b19301b7f1f
sybealla1 <138353366+sybealla1@users.noreply.github.com>
2025-01-15 18:26:11 +0800

    Update admin.php

--- admin.php
+++ admin.php
@@ -6,12 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Restaurant Website</title>
 
-
-
-
-
-
-
     <link rel="stylesheet" href="css/style.css">
 
     <style>
